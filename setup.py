85908e91004d7c5433ed56453c61a1c705262752
andygoldschmidt <andygoldschmidt@me.com>
2015-05-26 07:06:11 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from pip.req import parse_requirements
+import pypandoc
+from setuptools import setup
+
+parsed_reqs = parse_requirements('requirements.txt', session='foo')
+requirements = [str(p.req) for p in parsed_reqs]
+
+long_description = pypandoc.convert('README.md', 'rst')
+
+setup(
+    name='kiefer',
+    version=0.1,
+    description='A Python wrapper for the Jawbone UP API.',
+    long_description=long_description,
+    author='Andy Goldschmidt',
+    author_email='andy@abby.io',
+    license='MIT',
+    url='https://github.com/andygoldschmidt/kiefer',
+    install_requires=requirements
+)
